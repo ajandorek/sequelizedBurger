c4aa1b0021dc3ef43d7c978c5b6cb923263d566b
Osei Bonsu <osei.bonsu@gmail.com>
2017-02-22 11:55:21 -0600

    fix stuff

--- routes/burger-api-routes.js
+++ routes/burger-api-routes.js
@@ -1,32 +1,30 @@
 var db = require("../models");
 console.log("hi");
 
-module.exports = function(app) {
-    app.get("/api/burger", function(req, res){
+module.exports = function (app) {
+    app.get("/api/burger", function (req, res) {
         console.log("hello");
-        db.Burger.findAll({}).then(function(dbPost){
+        db.burger.findAll({}).then(function (dbPost) {
             res.json(dbPost);
         });
     });
 
-    app.post("/api/burger", function(req, res){
+    app.post("/api/burger", function (req, res) {
         console.log("Hello");
-        db.Burger.create({
-            burger_name: req.body
-        }).then(function(dbPost){
+        db.burger.create(req.body).then(function (dbPost) {
             res.json(dbPost);
         });
     });
 
-    app.put("/:id", function(req, res){
-        db.Burger.put(
+    app.put("/:id", function (req, res) {
+        db.burger.put(
             req.body,
             {
                 where: {
                     id: req.body.id
                 }
-            }).then(function(dbPost){
+            }).then(function (dbPost) {
                 res.json(dbPost);
-        });
+            });
     });
 }
